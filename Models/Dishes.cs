ada961e1c076f6f87323373b4a027c4c193a9e9d
Diwarii <yarmakmaslov@mail.ru>
2024-11-27 15:00:41 +0700

    Update 0.1.2

---
+++ Models/Dishes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ravintola.Models
+{
+    public class Dishes : Property
+    {
+        private uint _id;
+        public uint Id { get { return _id; } set { _id = value; OnPropertyChanged("Id"); } }
+
+        private string _foodName;
+        public string FoodName { get { return _foodName; } set { _foodName = value; OnPropertyChanged("FoodName"); } }
+
+        private double _foodCost;
+        public double FoodCost { get { return _foodCost; } set { _foodCost = value; OnPropertyChanged("FoodCost"); } }
+    }
+}
---
+++ Models/Products.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Navigation;
+
+namespace Ravintola.Models
+{
+    public class Products : Property
+    {
+        private uint _id;
+        public uint Id { get { return _id; } set { _id = value; OnPropertyChanged("Id"); } }
+
+        private string _name;
+        public string Name { get { return _name; } set { _name = value; OnPropertyChanged("Name"); } }
+
+        private double _count;
+        public double Count { get { return _count; } set { _count = value; OnPropertyChanged("Count"); } }
+    }
+}
