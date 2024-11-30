157d73392e0e94a6e0a3961357a58896e3c7ad18
Diwarii <yarmakmaslov@mail.ru>
2024-11-30 19:01:43 +0700

    update 0.2

---
+++ Models/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ravintola.Models
+{
+    public class ApplicationContext : DbContext // класс для создания БД
+    {
+
+        public DbSet<Dish> Dishes { get; set; } = null!;
+        public DbSet<Product> Products { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=RavintolaDB.db");
+        }
+    }
+}
